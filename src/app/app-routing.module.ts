1b8dbb33bba3d63616cdd1412d661acb41d8d5a4
luongquytnvn <luongquytnvn@gmail.com>
2019-12-26 17:39:45 +0700

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {BlogComponent} from './blog/blog.component';
-import {BlogDetailComponent} from './blog-detail/blog-detail.component';
-import {BlogEditComponent} from './blog-edit/blog-edit.component';
 import {CustomerComponent} from './customer/customer.component';
 import {CustomerCreateComponent} from './customer-create/customer-create.component';
 import {CustomerEditComponent} from './customer-edit/customer-edit.component';
 import {CustomerDeleteComponent} from './customer-delete/customer-delete.component';
 
 const routes: Routes = [{
-  path: 'blog',
-  component: BlogComponent
-}, {
-  path: 'blog/:id',
-  component: BlogDetailComponent
-}, {
-  path: 'blog/:id/edit',
-  component: BlogEditComponent
-}, {
   path: 'customers',
   component: CustomerComponent
 }, {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,10 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {ImageGalleryModule} from './image-gallery/image-gallery.module';
 import {AppComponent} from './app.component';
-import {GalleryConfig} from './image-gallery/token';
-import {ImgSliderComponent} from './img-slider/img-slider.component';
-import {BlogComponent} from './blog/blog.component';
-import {BlogEditComponent} from './blog-edit/blog-edit.component';
-import {BlogDetailComponent} from './blog-detail/blog-detail.component';
 import {RouterModule} from '@angular/router';
 import {ReactiveFormsModule} from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
-import {TodoComponent} from './todo/todo.component';
 import {CustomerComponent} from './customer/customer.component';
 import {CustomerCreateComponent} from './customer-create/customer-create.component';
 import {CustomerEditComponent} from './customer-edit/customer-edit.component';
@@ -20,7 +13,6 @@ import {CustomerDeleteComponent} from './customer-delete/customer-delete.compone
 @NgModule({
   imports: [
     BrowserModule,
-    ImageGalleryModule,
     RouterModule,
     HttpClientModule,
     ReactiveFormsModule,
@@ -28,20 +20,12 @@ import {CustomerDeleteComponent} from './customer-delete/customer-delete.compone
   ],
   declarations: [
     AppComponent,
-    ImgSliderComponent,
-    BlogComponent,
-    BlogEditComponent,
-    BlogDetailComponent,
-    TodoComponent,
     CustomerComponent,
     CustomerCreateComponent,
     CustomerEditComponent,
     CustomerDeleteComponent,
   ],
-  providers: [
-    {provide: GalleryConfig, useValue: 2},
-    // { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
